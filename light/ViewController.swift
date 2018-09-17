4e7135c9b875a13e4bb5509f0ff286062bcfd99d
Joe Eagar <joeeagar@joes-macbook-pro.local>
2018-09-17 07:59:50 -0600

    finished the light app

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    var lightOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
